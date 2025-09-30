4e48ac173be693216bec1b4a09e4cc8e595728f8
Thomasmp00 <thomaxx00@gmail.com>
2025-09-30 11:34:51 +0200

    Implements basic calculator functionality
    Adds basic addition functionality to the calculator.
    
    This commit implements the step definitions to add two numbers
    and verify the result. Removes the pending step exceptions
    and adds the necessary variables and a Calculator class instance.

--- CucumberTests/StepDefinitions/CalculatorStepDefinitions.cs
+++ CucumberTests/StepDefinitions/CalculatorStepDefinitions.cs
@@ -1,4 +1,4 @@
-using Reqnroll;
+using Reqnroll  ;
 
 namespace CucumberTests.StepDefinitions;
 
@@ -6,40 +6,30 @@ namespace CucumberTests.StepDefinitions;
 public sealed class CalculatorStepDefinitions
 {
     // For additional details on Reqnroll step definitions see https://go.reqnroll.net/doc-stepdef
+    int firstNumber, secondNumber, sum;
+    Calculator calculator = new Calculator();
 
     [Given("the first number is {int}")]
     public void GivenTheFirstNumberIs(int number)
     {
-        //TODO: implement arrange (precondition) logic
-        // For storing and retrieving scenario-specific data see https://go.reqnroll.net/doc-sharingdata
-        // To use the multiline text or the table argument of the scenario,
-        // additional string/DataTable parameters can be defined on the step definition
-        // method. 
-
-        throw new PendingStepException();
+        firstNumber = number;
     }
 
     [Given("the second number is {int}")]
     public void GivenTheSecondNumberIs(int number)
     {
-        //TODO: implement arrange (precondition) logic
-
-        throw new PendingStepException();
+        secondNumber = number;
     }
 
     [When("the two numbers are added")]
     public void WhenTheTwoNumbersAreAdded()
     {
-        //TODO: implement act (action) logic
-
-        throw new PendingStepException();
+        sum = calculator.Add(firstNumber, secondNumber);
     }
 
     [Then("the result should be {int}")]
     public void ThenTheResultShouldBe(int result)
     {
-        //TODO: implement assert (verification) logic
-
-        throw new PendingStepException();
+        Assert.Equal(expectedSum, sum);
     }
 }
